b60d91a4e9f2467fe210016d166c55686240a57d
Alexandra Szczerba <avsa2021@mymail.pomona.edu>
2024-04-02 11:47:14 +0200

    encode token

--- data/tokenizing/bert_tokenizing.py
+++ data/tokenizing/bert_tokenizing.py
@@ -10,6 +10,21 @@ tokens = tokenizer.tokenize("This is an example of the bert tokenizer")
 print(tokens)
 # ['this', 'is', 'an', 'example', 'of', 'the', 'bert', 'token', '##izer']
 
+token_ids = tokenizer.convert_tokens_to_ids(tokens)
+
+token_ids = tokenizer.encode("This is an example of the bert tokenizer")
+print(token_ids)
+
+tokens = tokenizer.convert_ids_to_tokens(token_ids)
+print(tokens)
+# ['[CLS]', 'this', 'is', 'an', 'example', 'of', 'the', 'bert', 'token', '##izer', '[SEP]']
+
+
+
+
+###^ encoding the tokens 
+
+
 
 import torch
 from transformers import BertModel
